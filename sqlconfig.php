7e709922e4abc5b2133428c073bcf9039c0eecf4
anna-hayoun <anna.hayoun@laplateforme.io>
2022-01-05 01:52:11 +0100

    final version - sqlconfig.php

---
+++ sqlconfig.php
@@ -0,0 +1,17 @@
+<?php
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'livreor');
+ 
+// Connexion à la base de données MySQL 
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Vérifier la connexion
+if($conn === false)
+{
+    die("ERREUR : Impossible de se connecter. " . mysqli_connect_error());
+}
+
+?>
